9329cf68f366cd738c467f1632d34741e8d29c9e
ccjmne <ccjmne@gmail.com>
2018-01-31 23:36:31 +0100

    Added CORS headers to Response

--- src/response.js
+++ src/response.js
@@ -1,7 +1,16 @@
 'use strict';
 
-module.exports = function (body = '', status = 200) {
+module.exports = function Response(body = '', status = 200) {
   this.statusCode = status;
   this.isBase64Encoded = false;
   this.body = JSON.stringify((typeof body === 'string') ? { message: body } : body, null, 2);
+  this.headers = {
+    'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+    'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
+    'Access-Control-Allow-Origin': '*'
+  };
+
+  Response.prototype.noCORS = function () {
+    return delete this.headers, this;
+  };
 };
