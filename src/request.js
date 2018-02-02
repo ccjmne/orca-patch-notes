dc58b64966cd7dfee6734405c78fd45a68df7175
ccjmne <ccjmne@gmail.com>
2018-02-02 14:16:14 +0100

    Version bump to 0.9.0-alpha.2

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function (event) {
+module.exports = function Request(event) {
   this.getPathParameter = name => event.pathParameters && event.pathParameters[name];
   this.getQueryParameter = name => event.queryStringParameters && event.queryStringParameters[name];
   this.getBody = () => event.body;
